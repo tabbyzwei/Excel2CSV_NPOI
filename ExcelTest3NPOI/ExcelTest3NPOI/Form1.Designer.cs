bcf01064369281c38d427fabbfa19fb96cd770a1
TabbyWang <wangzi@xiaomutech.com>
2016-07-12 16:40:16 +0800

    add project to git

---
+++ ExcelTest3NPOI/ExcelTest3NPOI/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ExcelTest3NPOI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.ofdOpenExcel = new System.Windows.Forms.OpenFileDialog();
+            this.btnCreateFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(258, 12);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(124, 60);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "OpenFile";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Visible = false;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // ofdOpenExcel
+            // 
+            this.ofdOpenExcel.FileName = "ofdOpenExcel";
+            this.ofdOpenExcel.Filter = "Excel|*.xlsx";
+            // 
+            // btnCreateFile
+            // 
+            this.btnCreateFile.Location = new System.Drawing.Point(12, 12);
+            this.btnCreateFile.Name = "btnCreateFile";
+            this.btnCreateFile.Size = new System.Drawing.Size(148, 60);
+            this.btnCreateFile.TabIndex = 1;
+            this.btnCreateFile.Text = "CreateFile";
+            this.btnCreateFile.UseVisualStyleBackColor = true;
+            this.btnCreateFile.Click += new System.EventHandler(this.btnCreateFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 84);
+            this.Controls.Add(this.btnCreateFile);
+            this.Controls.Add(this.btnOpenFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Excel2CSVTool";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.OpenFileDialog ofdOpenExcel;
+        private System.Windows.Forms.Button btnCreateFile;
+    }
+}
+
---
+++ ExcelTest3NPOI/ExcelTest3NPOI/Form1.cs
@@ -0,0 +1,129 @@
+﻿using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ExcelTest3NPOI
+{
+    public partial class Form1 : Form
+    {
+        String pathFile = "";
+        String pathDirection = "";
+        String pathOutputDirection = "";
+        String fileName = "";
+
+        IWorkbook wbSource = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text += " Ver0.2.20160712.1";
+        }
+
+        public void fileProcess(String filePath)
+        {
+            int srcSheetCount;
+
+            MemoryStream ms = new MemoryStream();
+
+            FileInfo fiSource = new FileInfo(filePath);
+            //if file exist
+            if (fiSource.Exists)
+            {
+                //open file and get file stream
+                FileStream fsSource = fiSource.OpenRead();
+                if (fiSource.Extension.Equals(".xls"))
+                {
+                    wbSource = new HSSFWorkbook(fsSource);
+                }
+                else if (fiSource.Extension.Equals(".xlsx"))
+                {
+                    wbSource = new XSSFWorkbook(fsSource);
+                }
+                //close file stream
+                fsSource.Close();
+
+                //get sheet count
+                srcSheetCount = wbSource.NumberOfSheets;
+                ISheet usingSheet = wbSource.GetSheetAt(0);
+                IRow rowHeader = usingSheet.GetRow(0);
+                if (rowHeader != null) {
+                    ICell cellHeader = rowHeader.GetCell(0);
+                    cellHeader.SetCellValue("Test");
+                }
+                //usingSheet.CreateRow(0).CreateCell(0).SetCellValue("0");
+
+                wbSource.Write(ms);
+
+                using (FileStream fs = new FileStream("123.xlsx", FileMode.Create, FileAccess.Write))
+                {
+                    byte[] data = ms.ToArray();
+                    fs.Write(data, 0, data.Length);
+                    fs.Flush();
+                    data = null;
+                }
+            }
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            if (ofdOpenExcel.ShowDialog() == DialogResult.OK)
+            {
+                if (ofdOpenExcel.CheckFileExists)
+                {
+                    //get file path
+                    pathFile = ofdOpenExcel.FileName;
+                    //get Direction
+                    pathDirection = System.IO.Path.GetDirectoryName(pathFile);
+                    //get file name
+                    fileName = System.IO.Path.GetFileNameWithoutExtension(pathFile);
+                    //debug output file name
+                    System.Diagnostics.Debug.WriteLine("\r\nDebug Output:\r\n" + pathFile + "\r\n");
+                    //process the file
+                    fileProcess(pathFile);
+                }
+                else
+                {
+                    MessageBox.Show("No File");
+                }
+            }
+        }
+
+        private void btnCreateFile_Click(object sender, EventArgs e)
+        {
+            XSSFWorkbook workbook = new XSSFWorkbook();
+            MemoryStream ms = new MemoryStream();
+
+            // 新增試算表。 
+            ISheet sheet = workbook.CreateSheet("SheetA");
+            //sheet.GetRow(0).GetCell(0);
+            sheet.CreateRow(0).CreateCell(0).SetCellValue("想吃干馏抄手");
+            workbook.CreateSheet("SheetB");
+            workbook.CreateSheet("SheetC");
+
+            workbook.Write(ms);
+            using (FileStream fs = new FileStream(@"D:\想吃干馏抄手.xlsx", FileMode.Create, FileAccess.Write))
+            {
+                byte[] data = ms.ToArray();
+                fs.Write(data, 0, data.Length);
+                fs.Flush();
+                data = null;
+            }
+
+            workbook = null;
+            ms.Close();
+            ms.Dispose();
+            MessageBox.Show("想吃干馏抄手");
+            //open explorer
+            System.Diagnostics.Process.Start("explorer.exe", @"D:\");
+        }
+    }
+}
