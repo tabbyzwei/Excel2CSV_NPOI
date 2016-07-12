a1fc02d9127dd5f32bd3cf447511e08af9712f8b
TabbyWang <wangzi@xiaomutech.com>
2016-07-12 18:36:02 +0800

    add sheetInfo.cs

---
+++ ExcelTest3NPOI/ExcelTest3NPOI/sheetInfo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExcelTest3NPOI
+{
+    public class sheetInfo
+    {
+        private int rowMax;
+        private int listMax;
+        private int[] rowIndex = new int[10];
+        #region getter,setter
+        public int RowMax
+        {
+            get
+            {
+                return rowMax;
+            }
+
+            set
+            {
+                rowMax = value;
+            }
+        }
+
+        public int ListMax
+        {
+            get
+            {
+                return listMax;
+            }
+
+            set
+            {
+                listMax = value;
+            }
+        }
+
+        public int[] RowIndex
+        {
+            get
+            {
+                return rowIndex;
+            }
+
+            set
+            {
+                rowIndex = value;
+            }
+        }
+
+
+        #endregion
+    }
+}
