ccb0fc35ddd33514f99d472beae786037b55c980
TabbyWang <wangzi@xiaomutech.com>
2016-07-13 11:24:34 +0800

    pend Coding because the Color Problem in NPOI

--- ExcelTest3NPOI/ExcelTest3NPOI/Form1.cs
+++ ExcelTest3NPOI/ExcelTest3NPOI/Form1.cs
@@ -9,6 +9,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace ExcelTest3NPOI
@@ -19,20 +20,22 @@ namespace ExcelTest3NPOI
         String pathDirection = "";
         String pathOutputDirection = "";
         String fileName = "";
+        String ErrorMessage = "";
+
+        String[] headerTable = { "Status", "ODM", "Vendor","RMA No.","AcerP/N",
+            "QTY","ODM U/P","Vendor C/N","Vendor U/P","MM#/ID"};
 
         IWorkbook wbSource = null;
 
         public Form1()
         {
             InitializeComponent();
-            this.Text += " Ver0.2.20160712.1";
+            this.Text += " Ver0.2.20160712.2";
         }
 
         public void fileProcess(String filePath)
         {
             int srcSheetCount;
-
-            MemoryStream ms = new MemoryStream();
 
             FileInfo fiSource = new FileInfo(filePath);
             //if file exist
@@ -53,24 +56,115 @@ namespace ExcelTest3NPOI
 
                 //get sheet count
                 srcSheetCount = wbSource.NumberOfSheets;
-                ISheet usingSheet = wbSource.GetSheetAt(0);
-                IRow rowHeader = usingSheet.GetRow(0);
-                if (rowHeader != null) {
-                    ICell cellHeader = rowHeader.GetCell(0);
-                    cellHeader.SetCellValue("Test");
+                //get Every Sheet
+                for (int i = 0; i < srcSheetCount; i++)
+                {
+                    ISheet srcSheet = wbSource.GetSheetAt(i);
+                    sheetProcess(srcSheet);
                 }
+
+                if (!ErrorMessage.Equals(""))
+                {
+                    MessageBox.Show(ErrorMessage);
+                }
+
+                //ISheet usingSheet = wbSource.GetSheetAt(0);
+                //IRow rowHeader = usingSheet.GetRow(0);
+                //if (rowHeader != null) {
+                //    ICell cellHeader = rowHeader.GetCell(0);
+                //    cellHeader.SetCellValue("Test");
+                //}
                 //usingSheet.CreateRow(0).CreateCell(0).SetCellValue("0");
 
-                wbSource.Write(ms);
 
-                using (FileStream fs = new FileStream("123.xlsx", FileMode.Create, FileAccess.Write))
+                //using (FileStream fs = new FileStream(@"D:\123.xlsx", FileMode.Create, FileAccess.Write))
+                //{
+                //    wbSource.Write(fs);
+                //}
+
+                GC.Collect();
+            }
+        }
+        public void sheetProcess(ISheet Sheet)
+        {
+            String strTemp = "";
+            IRow row;
+            
+            short indexColor;
+
+            //get sheet message
+            sheetInfo si = sheetParse(Sheet);
+            //create workbook
+            XSSFWorkbook workbook = new XSSFWorkbook();
+
+            //if error show error message
+            if (!si.ErrorMessage.Equals(""))
+            {
+                ErrorMessage += Sheet.SheetName + ": " + si.ErrorMessage + "\r\n";
+            }
+            //no error,copy sheet to new sheet
+            else
+            {
+                for (int i = 1; i < si.RowMax; i++)
                 {
-                    byte[] data = ms.ToArray();
-                    fs.Write(data, 0, data.Length);
-                    fs.Flush();
-                    data = null;
+                    //row = Sheet.GetRow(i);
+                    ICell cell = Sheet.GetRow(i).GetCell(si.RowIndex[0]);
+                    if (cell != null)
+                    {
+                        strTemp = cell.StringCellValue;
+                        ICellStyle style = cell.CellStyle;
+                        IColor color = style.FillForegroundColorColor;
+                        byte[] RGB = IndexedColors.Yellow.RGB;
+                        if (color != null)
+                        {
+
+                            if (strTemp.Equals("open") && RGB[0]==color.RGB[0] && RGB[1] == color.RGB[1] && RGB[2] == color.RGB[2])
+                            {
+                                //strTemp = row.GetCell(si.RowIndex[7]).StringCellValue;
+                                if (!strTemp.Equals(""))
+                                {
+
+                                }
+                            }
+                        }
+                    }
                 }
             }
+        }
+        public sheetInfo sheetParse(ISheet Sheet)
+        {
+            sheetInfo si = new sheetInfo();
+            
+            IRow row = Sheet.GetRow(0);
+            si.RowMax = Sheet.LastRowNum;
+            si.ListMax = row.LastCellNum;
+
+            bool headerFoundFlag = false;
+
+            String strTemp = "";
+            for (int i = 0; i < headerTable.Length; i++)
+            {
+                //find header name in first row
+                for (int j = 0; j < si.ListMax; j++)
+                {
+                    ICell cell = row.GetCell(j);
+                    strTemp = cell.StringCellValue;
+                    if (strTemp.Equals(headerTable[i]))
+                    {
+                        si.RowIndex[i] = j;
+                        headerFoundFlag = true;
+                        break;
+                    }
+                }
+                //header name not find
+                if (!headerFoundFlag)
+                {
+                    si.ErrorMessage += headerTable[i] + " No Found; ";
+                }
+                //reset flag
+                headerFoundFlag = false;
+            }
+            return si;
         }
 
         private void btnOpenFile_Click(object sender, EventArgs e)
@@ -85,8 +179,10 @@ namespace ExcelTest3NPOI
                     pathDirection = System.IO.Path.GetDirectoryName(pathFile);
                     //get file name
                     fileName = System.IO.Path.GetFileNameWithoutExtension(pathFile);
-                    //debug output file name
-                    System.Diagnostics.Debug.WriteLine("\r\nDebug Output:\r\n" + pathFile + "\r\n");
+                    //output direction
+                    pathOutputDirection = pathDirection + "\\" + fileName;
+                    //debug output file
+                    System.Diagnostics.Debug.WriteLine("\r\nDebug Output:\r\n" + pathOutputDirection + "\r\n");
                     //process the file
                     fileProcess(pathFile);
                 }
